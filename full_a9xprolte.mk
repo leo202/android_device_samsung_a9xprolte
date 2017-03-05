8141a86f124f567347127ddc8614c00bf4edec63
leo202 <leoz2tablet@gmail.com>
2017-03-05 11:34:51 +0700

    use fullbase telephony

--- full_a9xprolte.mk
+++ full_a9xprolte.mk
@@ -16,7 +16,8 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 
 # Inherit from a9xprolte device
 $(call inherit-product, device/samsung/a9xprolte/device.mk)
